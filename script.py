87aee8d31ace681f18bac2fd0f52672ed0361c05
tyheise <tyler.heise@gmail.com>
2019-03-31 21:00:33 -0600

    Added script to get the commit for each release.

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import os
 import sys
 import getpass
 from github import Github
+from datetime import datetime
 
 def searchRepos(maxNumOfRepos):
     git = Github()
@@ -31,7 +32,7 @@ def getReleaseCSVs():
             repoName = ((repo.full_name).split("/"))[1]
 
             releaseDates = open("releaseCSVs/"+repoName+".csv", "w")
-            releaseDates.write("id,tag_name,until,since,dayDifference\n")
+            releaseDates.write("repo,id,tag_name,until,since,dayDifference\n")
             prevDate = None
             for i in range(releaseCount - 1):
                 currRelease = releases[i]
@@ -42,12 +43,41 @@ def getReleaseCSVs():
 
                 dayDifference = currCreatedAt - prevCreatedAt
 
-                releaseDates.write(str(currRelease.id)+","+currRelease.tag_name+","+str(currCreatedAt)+","+str(prevCreatedAt)+","+str(dayDifference)+"\n")
+                releaseDates.write(repo.full_name+','+str(currRelease.id)+","+currRelease.tag_name+","+str(currCreatedAt)+","+str(prevCreatedAt)+","+str(dayDifference)+"\n")
 
         line = reposFile.readline().strip()
 
     reposFile.close()
 
+def getCommits():
+    username = input("Github Username:")
+    pw = getpass.getpass()
+    git = Github(username, pw)
+
+    csvFolder = os.path.join(os.path.curdir, "releaseCSVs")
+    fileList = os.listdir(csvFolder)
+
+    for fileName in fileList:
+        csvPath = os.path.join(csvFolder, fileName)
+        csv = open(csvPath)
+
+        lines = csv.readlines()
+        repoName = (lines[1].split(","))[0]
+        repo = git.get_repo(repoName)
+
+        repoName2 = (repoName.split("/"))[1]
+
+        repoFile = open(os.path.join(os.path.curdir,'commits',repoName2+".csv"), 'w')
+        for i in range(1,len(lines)-1):
+            line = lines[i].split(",")
+
+            # 2019-03-26 15:08:13
+            until = datetime.strptime(line[3], '%Y-%m-%d %H:%M:%S')
+            since = datetime.strptime(line[4], '%Y-%m-%d %H:%M:%S')
+
+            commits = repo.get_commits(until=until, since=since)
+            if commits.totalCount > 0:
+                repoFile.write(line[1]+","+line[2]+","+commits[0].sha+","+str(until-since)+"\n")
 
 def get_repo(repoName, git):
     repo = git.get_repo(repoName)
@@ -59,21 +89,6 @@ def main(argv):
     pw = getpass.getpass()
     git = Github(username, pw)
 
-
-    # orgName = argv[0]
-    # repoName = argv[1]
-    #
-    # git = Github()
-    # org = git.get_organization(orgName)
-    # repo = org.get_repo(repoName)
-    #
-    # print(repo.html_url)
-    #
-    # releases = repo.get_releases()
-    # print(releases.totalCount)
-    #
-    # #for release in releases:
-
     return
 
 if __name__== "__main__":
@@ -83,5 +98,7 @@ if __name__== "__main__":
             searchRepos(int(sys.argv[2]))
         elif arg == 'csv':
             getReleaseCSVs()
+        elif arg == 'commits':
+            getCommits()
     else:
         main(sys.argv[1:])
