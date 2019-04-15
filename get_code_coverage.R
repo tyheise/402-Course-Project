573e45a967dd6104ac44f1b0ae5dcb5d9619e2b8
pennyfea <pennyfea@ualberta.ca>
2019-04-15 00:42:09 -0600

    Fixed the script and added the png files

--- get_code_coverage.R
+++ get_code_coverage.R
@@ -9,9 +9,10 @@ csv_list = list.files(pattern="*.csv")
 print(csv_list)
 
 
-png_csv_path= (paste(getwd(),"codecov_png",sep="/"))
+png_csv_path <- (paste(getwd(),"codecov_png",sep="/"))
 dir.create(png_csv_path)
 print(png_csv_path)
+
 
 
 
@@ -33,7 +34,7 @@ for(i in 1:length(csv_list)){
   
   #ggsave(filename = sub('\\.csv$', '.png', csv_list[i]), device = "png", plot = gg) # change file extension to indicate output format
   #message(file.path(png_csv_path, sub('\\.csv$', '.png', basename(csv_list[i]))))
-  ggsave(filename = file.path(png_csv_path, sub('\\.csv$', '.png', basename(csv_list[i])), device ="png", plot = gg))
+  ggsave(filename = file.path(png_csv_path, sub('\\.csv$', '.png', basename(csv_list[i]))), device ="png", plot = gg)
 }
 
 
